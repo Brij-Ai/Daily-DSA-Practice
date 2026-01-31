82344bf819071a1aff303acd760c8fa26fc688d4
BrijK <Brijk0625@gmail.com>
2026-01-31 18:17:31 +0530

    Own_Logic-And-Optimal-Solution

--- LeetCode/Arrays/LC_1207-Unique-NumberOfOccurences.cpp
+++ LeetCode/Arrays/LC_1207-Unique-NumberOfOccurences.cpp
@@ -9,6 +9,8 @@ Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values h
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
 using namespace std;
 class Solution
 {
@@ -27,6 +29,67 @@ public:
             {
                 return false;
             }
+        }
+        return true;
+    }
+};
+
+// Approach 2.
+/*
+Time complexity: O(nlogn)+O(k) //sorting then freqency count
+Space complexity: O(n) No extra space
+*/
+// Own logic
+class Solution
+{
+public:
+    bool uniqueOccurrences(vector<int> &arr)
+    {
+        unordered_set<int> st;
+        sort(arr.begin(), arr.end());
+        int count = 0;
+        int curr = arr[0];
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (curr == arr[i])
+            {
+                count++;
+            }
+            else if (st.count(count))
+                return false;
+            else
+            {
+                st.insert(count);
+                count = 1;
+                curr = arr[i];
+            }
+        }
+        if (st.count(count))
+            return false;
+        else
+            st.insert(count);
+        return true;
+    }
+};
+
+// Approach Most optimal
+// Time: O(n)
+// Space: O(n)
+class Approach
+{
+public:
+    bool uniqueOccurrences(vector<int> &arr)
+    {
+        unordered_map<int, int> freq;
+        for (int x : arr)
+            freq[x]++;
+
+        unordered_set<int> st;
+        for (auto &p : freq)
+        {
+            if (st.count(p.second))
+                return false;
+            st.insert(p.second);
         }
         return true;
     }
