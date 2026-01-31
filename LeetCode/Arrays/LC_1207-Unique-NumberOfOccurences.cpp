25c97db2c2cf9e8fdfe43df6242600929a101263
BrijK <Brijk0625@gmail.com>
2026-01-31 15:04:17 +0530

    Unique-Number-Of-Occurrences

---
+++ LeetCode/Arrays/LC_1207-Unique-NumberOfOccurences.cpp
@@ -0,0 +1,33 @@
+/*
+Description: Given an array of integers arr,
+return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    bool uniqueOccurrences(vector<int> &arr)
+    {
+        vector<int> ca(2001);
+        for (int i : arr)
+        {
+            ca[i + 1000]++;
+        }
+        sort(ca.begin(), ca.end());
+        for (int i = 0; i < 2000; i++)
+        {
+            if (ca[i] != 0 && ca[i] == ca[i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
