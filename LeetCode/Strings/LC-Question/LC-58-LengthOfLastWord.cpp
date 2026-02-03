41ab57db23bf51b2cf325de041c8c89ba591e3ca
BrijK <Brijk0625@gmail.com>
2026-02-03 17:04:42 +0530

    first-LeetCode-problem:::::

---
+++ LeetCode/Strings/LC-Question/LC-58-LengthOfLastWord.cpp
@@ -0,0 +1,38 @@
+/*
+Description: Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+            A word is a maximal substring consisting of non-space characters only.
+
+Example 1:
+Input:s = "Hello World"
+        Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+        Input: s = "   fly me   to   the moon  "
+        Output: 4
+Explanation: The last word is "moon" with length 4
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+// Solution 1:
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int i = s.length() - 1;
+        // Step 1: Skip trailing spaces
+        while (i >= 0 && s[i] == ' ')
+            i--;
+        // Step 2: Count characters of last word
+        int count = 0;
+        while (i >= 0 && s[i] != ' ')
+        {
+            count++;
+            i--;
+        }
+        return count;
+    }
+};
