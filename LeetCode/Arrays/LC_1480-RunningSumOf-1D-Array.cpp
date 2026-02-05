0686261c0219ad7be8784e22465507df00b3d31a
BrijK <Brijk0625@gmail.com>
2026-02-05 17:11:11 +0530

    Running_------

---
+++ LeetCode/Arrays/LC_1480-RunningSumOf-1D-Array.cpp
@@ -0,0 +1,31 @@
+/*
+Description: Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+            Return the running sum of nums.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+        Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+// Solution:
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (i != 0)
+            {
+                nums[i] += nums[i - 1];
+            }
+        }
+        return nums;
+    }
+};
