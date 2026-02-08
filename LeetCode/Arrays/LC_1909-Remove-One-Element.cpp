9de820002175b01f745f31796b803ff27ca4bd14
BrijK <Brijk0625@gmail.com>
2026-02-08 18:44:17 +0530

    Sloved-Wih-The-Optimal-Approach...

--- LeetCode/Arrays/LC_1909-Remove-One-Element.cpp
+++ LeetCode/Arrays/LC_1909-Remove-One-Element.cpp
@@ -37,3 +37,19 @@ public:
         return true;
     }
 };
+
+// Solution 2: O(n) time, O(1) space
+bool canBeIncreasing(vector<int> &nums)
+{
+    int cnt = 0;
+    for (int i = 1; i < nums.size() && cnt < 2; ++i)
+    {
+        if (nums[i - 1] >= nums[i])
+        {
+            ++cnt;
+            if (i > 1 && nums[i - 2] >= nums[i])
+                nums[i] = nums[i - 1];
+        }
+    }
+    return cnt < 2;
+}
