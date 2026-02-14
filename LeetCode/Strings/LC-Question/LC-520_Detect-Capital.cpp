db30db23665c6952ac4887ee95ce99262ee5973e
BrijK <Brijk0625@gmail.com>
2026-02-14 19:44:18 +0530

    Add solution for Detect Capital problem with two approaches

---
+++ LeetCode/Strings/LC-Question/LC-520_Detect-Capital.cpp
@@ -0,0 +1,67 @@
+// Description: https://leetcode.com/problems/detect-capital/
+#include <string>
+using namespace std;
+
+// Solution 1: Two Pointers
+class Solution
+{
+public:
+    bool detectCapitalUse(string word)
+    {
+        int n = word.size();
+        if (n == 0)
+            return true;
+        if (n == 1)
+            return true;
+        if (isupper(word[0]) && isupper(word[1]))
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (!isupper(word[i]))
+                    return false;
+            }
+        }
+        else
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (isupper(word[i]))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+
+class Solution
+{
+public:
+    bool detectCapitalUse(string word)
+    {
+        int n = word.size();
+
+        if (n == 1)
+            return true;
+
+        // Case 1: First two letters uppercase
+        if (isupper(word[0]) && isupper(word[1]))
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (!isupper(word[i]))
+                    return false;
+            }
+        }
+        else
+        {
+            // All remaining letters must be lowercase
+            for (int i = 1; i < n; i++)
+            {
+                if (!islower(word[i]))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
